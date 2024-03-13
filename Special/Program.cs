da63b62cefa1c5e698d9f79212b56a653be30632
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-03-13 12:34:19 +0000

    Add recursive method

--- Special/Program.cs
+++ Special/Program.cs
@@ -7,6 +7,23 @@ namespace Special
         static void Main(string[] args)
         {
             Console.Write("Insert n-th number of the special sequence: ");
+            string str = Console.ReadLine();
+            int num = int.Parse(str);
+            Console.WriteLine(GetSpecial(num));
+        }
+        private static int GetSpecial(int n)
+        {
+            int result;
+            if (n == 0)
+                result = 0;
+            else if (n == 1 || n == 2)
+                result = 1;
+            else
+            {
+                result = GetSpecial(n - 2) + (2 * GetSpecial(n - 1));
+            }
+
+            return result;
         }
     }
 }
