65f0e75ee3ba8059f9aa58927450d015607ebf3f
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-03-13 12:17:39 +0000

    add question to user

---
+++ Special/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Special
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert n-th number of the special sequence: ");
+        }
+    }
+}
